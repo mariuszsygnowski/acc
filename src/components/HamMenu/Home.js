850f1e4d46d7ae3974db258e19c2dbb12217f2a8
Mariusz Sygnowski <sygnowski.m@gmail.com>
2019-01-28 20:15:51 +0000

    changes in home component

--- src/components/HamMenu/Home.js
+++ src/components/HamMenu/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Image, Transformation } from "cloudinary-react";
 
 import "./css/Home.css";
 
@@ -32,38 +31,36 @@ export default class Home extends React.Component {
             <h2>GABHEN ACC LIMITED</h2>
           </div>
         </div>
-
-        <p>
-          GABHEN ACC LIMITED has a growing reputation as one of the region's
-          most effective accountancy firms for individual clients and
-          businesses.
-        </p>
-
-        <p>
-          {" "}
-          Our firm expertly attends to all your taxation and business queries.
-          Thanks to the established expertise of our experts, our dedication and
-          many years of experience in consulting and assessment, we can offer
-          you support that you can rely on both with standard matters concerning
-          your enterprise and complex business management assignments. We thus
-          value highly the loyal consultancy of our clients.
-        </p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
-        <p>Home</p>
+        <div className="home__para">
+          <p>
+            GABHEN ACC LIMITED has a growing reputation as one of the region's
+            most effective accountancy firms for individual clients and
+            businesses.
+          </p>
+          <p>
+            Our firm expertly attends to all your taxation and business queries.
+            Thanks to the established expertise of our experts, our dedication
+            and many years of experience in consulting and assessment, we can
+            offer you support that you can rely on both with standard matters
+            concerning your enterprise and complex business management
+            assignments. We thus value highly the loyal consultancy of our
+            clients.
+          </p>
+          <p>
+            What makes us special Quality consultancy based on experience: learn
+            all about our firm's philosophy, priorities and tradition. About our
+            company What we offer From taxation and business management
+            consultancy right through to legal affairs and business management
+            assessment: we offer you comprehensive and individual support on all
+            taxation and business management matters. Our services
+          </p>
+          <p>
+            What we offer From taxation and business management consultancy
+            right through to legal affairs and business management assessment:
+            we offer you comprehensive and individual support on all taxation
+            and business management matters.
+          </p>
+        </div>
       </div>
     );
   }
